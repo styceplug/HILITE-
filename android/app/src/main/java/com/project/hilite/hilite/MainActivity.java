a1ad3a38335389c14e9317e52fc8d88b93aa617f
Styce <styceplug@gmail.com>
2026-04-27 22:52:42 +0100

    chore: Update project version and cleanup Android MainActivity files
    This commit increments the project build version and removes redundant or incorrectly placed `MainActivity` files from the Android source directory.
    
    **Key Changes:**
    
    - **Android Configuration:**
        - Deleted `MainActivity.java` from the `com.project.hilite.hilite` package.
        - Deleted `MainActivity.kt` from the `com.cube.cubeofelecktrike` package.
    
    - **Project Configuration (`pubspec.yaml`):**
        - Incremented build version to `1.0.0+20`.

--- android/app/src/main/java/com/project/hilite/hilite/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.project.hilite.hilite;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/com/cube/cubeofelecktrike/MainActivity.kt
+++
@@ -1,4 +0,0 @@
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity() {
-}
